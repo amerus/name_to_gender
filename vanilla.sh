1982825b890c1db95361d7639c70a370ef2c8a4d
amerus <sergey.motorny@vanderbilt.edu>
2021-07-03 15:58:57 -0500

    Jupyter Notebook and vanilla script update

--- vanilla.sh
+++ vanilla.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8180:8180 -p 8181:8181 -v /home/motorns/Documents/geekbrains/BusinessML/project/app/models:/app/app/models --name final amerus/name_to_gender:latest
+docker run -d -p 8180:8180 -p 8181:8181 -v `pwd`/model:/app/app/models --name name_to_gender amerus/name_to_gender:latest
